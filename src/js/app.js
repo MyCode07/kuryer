4b63ce19bc43077464e10853c6a22c333224c269
MyCode07 <erem19992015@mail.ru>
2023-11-02 19:14:58 +0300

    add to top btn

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import "./parts/menu.js";
 import "./parts/range.js";
 import { maskInputs } from "./static/inputmask.js";
 import { accorden } from "./static/accordeon.js";
+import { toTop } from "./static/to-top.js";
 
 maskInputs('+7 (999) 999-99-99', 'input[name="phone"]')
 accorden();
+toTop();
